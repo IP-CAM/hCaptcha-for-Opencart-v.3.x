143c1dc40d73667f40772380e7be0c283647e7b3
Sigfried Seldeslachts <sigfried@iswleuven.be>
2020-09-03 14:49:51 +0200

    Added all code

---
+++ upload/admin/language/en-gb/extension/captcha/hcaptcha.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'hCaptcha';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have updated the settings!';
+$_['text_edit']        = 'Edit hCaptcha';
+$_['text_signup']      = 'Go to <a href="https://www.hcaptcha.com/" target="_blank" rel="noopener noreferrer"><u>hCaptcha home page</u></a> and register your website.';
+
+// Entry
+$_['entry_key']        = 'Site key';
+$_['entry_secret']     = 'Secret key';
+$_['entry_ipaddress']  = 'Send user IP to hCaptcha';
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify hCaptcha!';
+$_['error_key']        = 'Key required!';
+$_['error_secret']     = 'Secret required!';
+$_['error_ipaddress']  = 'IP Address status required!';
